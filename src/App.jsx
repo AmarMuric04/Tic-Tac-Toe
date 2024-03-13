b122f23a36dfec399bc66c7984cb349fa08ebb91
AmarMuric04 <muricamar2004@gmail.com>
2024-03-13 15:44:24 +0100

    Clicking on squares adds an X

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
-
   return (
     <main>
       <div id="game-container">
@@ -9,6 +9,7 @@ function App() {
           <Player name="Murga" move="X" />
           <Player name="Agrum" move="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,27 +1,40 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleClickedField(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      /*prettier-ignore */
+      const newGameBoard = [...prevGameBoard.map(innerArray=> [...innerArray])];
+
+      newGameBoard[rowIndex][colIndex] = "X";
+      return newGameBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      <li>
-        <ol>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ol>
-      </li>
-      <li>
-        <ol>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ol>
-      </li>
-      <li>
-        <ol>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ol>
-      </li>
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li
+                onClick={handleClickedField.bind(null, rowIndex, colIndex)}
+                key={colIndex}
+              >
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
   );
 }
